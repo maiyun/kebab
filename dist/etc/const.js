b3254f019d7c3335fa260524b73e8bcced69c30e
yunboo <zohegs@gmail.com>
2019-04-02 17:16:02 +0800

    commit.

--- dist/etc/const.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VER = "0.0.1";
-exports.TOP_PATH = __dirname.substr(0, __dirname.lastIndexOf("/") + 1);
-exports.ROOT_PATH = __dirname + "/";
-exports.ETC_PATH = exports.ROOT_PATH + "etc/";
-exports.LIB_PATH = exports.ROOT_PATH + "lib/";
