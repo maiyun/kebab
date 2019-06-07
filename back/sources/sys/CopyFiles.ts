0d275dfe3a80a0a1cdf444b6a9236e588d182261
yunboo <zohegs@gmail.com>
2019-06-07 17:53:37 +0800

    Fix CopyFiles async bug.

--- back/sources/sys/CopyFiles.ts
+++ back/sources/sys/CopyFiles.ts
@@ -1,5 +1,6 @@
 import * as Fs from "../lib/Fs";
-import * as c from "../const";
+import * as Const from "../const";
+import * as abs from "../abstract";
 
 /**
  * --- 本文件用于复制 front 静态文件到 dist 目录，sources 里的静态文件到 dist 目录 ---
@@ -8,21 +9,33 @@ import * as c from "../const";
 export async function run() {
     let acList = [
         // --- back 里的系统级别的静态复制到 dist，如 Net 库的 pem 静态文件 ---
-        {from: c.SOURCES_PATH, to: c.DIST_PATH}
+        {from: Const.SOURCES_PATH, to: Const.DIST_PATH}
     ];
-    let list = await Fs.readDir(c.FRONT_PATH);
-    for (let item of list) {
-        if (item === "." || item === "..") {
+    // --- 加载 VHOST 信息 ---
+    let vhosts: abs.Vhost[] = [];
+    let files = await Fs.readDir(Const.VHOST_PATH);
+    for (let file of files) {
+        let fstr = await Fs.readFile(Const.VHOST_PATH + file);
+        if (fstr === undefined) {
             continue;
         }
+        let list: abs.Vhost[] = JSON.parse(fstr);
+        if (!Array.isArray(list)) {
+            list = [list];
+        }
+        for (let item of list) {
+            vhosts.push(item);
+        }
+    }
+    for (let vhost of vhosts) {
         acList.push({
-            from: c.FRONT_PATH + item + "/stc/", to: c.WWW_PATH + item + "/stc/"
+            from: Const.FRONT_PATH + vhost.root + "stc/", to: Const.WWW_PATH + vhost.root + "stc/"
         });
         acList.push({
-            from: c.FRONT_PATH + item + "/view/", to: c.WWW_PATH + item + "/view/"
+            from: Const.FRONT_PATH + vhost.root + "view/", to: Const.WWW_PATH + vhost.root + "view/"
         });
         acList.push({
-            from: c.FRONT_PATH + item + "/locale/", to: c.WWW_PATH + item + "/locale/"
+            from: Const.FRONT_PATH + vhost.root + "locale/", to: Const.WWW_PATH + vhost.root + "locale/"
         });
     }
 
@@ -32,6 +45,6 @@ export async function run() {
         await Fs.sync(item.from, item.to);
     }
 
-    await Fs.mkdir(c.FTMP_PATH);
+    await Fs.mkdir(Const.FTMP_PATH);
     console.log("Done.");
 }
