c577f98fdd2e523bd2f9999b4c13b69c4a17498f
yunbookf <zohegs@gmail.com>
2023-12-02 21:50:48 +0800

    Op child.

--- sys/child.ts
+++ sys/child.ts
@@ -88,7 +88,8 @@ async function run(): Promise<void> {
                 certHostIndex[servername] = i;
                 return;
             }
-            console.log('CERT NOT FOUND', servername);
+            const err = new Error('CERT NOT FOUND');
+            cb(err);
         },
         'ciphers': 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS',
         'allowHTTP1': true
@@ -131,6 +132,9 @@ async function run(): Promise<void> {
                 'input': ''
             }, '[child][http2][request]' + JSON.stringify((e.stack as string)).slice(1, -1), '-error');
         });
+    }).on('tlsClientError', (err, socket) => {
+        console.log('[child][http2][tls]', err);
+        socket.destroy();
     }).on('upgrade', function(req: http.IncomingMessage, socket: tls.TLSSocket): void {
         const host = (req.headers['host'] ?? '');
         if (!host) {
