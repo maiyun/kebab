2ba5e00bdf0bf3d31b24cfbccea37d5406c09cc1
han <zohegs@gmail.com>
2025-04-21 02:30:14 +0800

    op.

--- lib/lan.ts
+++ lib/lan.ts
@@ -4,50 +4,62 @@ import * as lCore from '~/lib/core';
 /**
  * --- 获取当前网卡的 IP、MAC 信息 ---
  */
-export function card(): Array<{
+export async function card(): Promise<Array<{
     'name': string;
     'mac': string;
     'iPv4': string;
     'iPv6': string;
-}> {
+}>> {
     const result: Array<{
         'name': string;
         'mac': string;
         'iPv4': string;
         'iPv6': string;
     }> = [];
-    const nifs = os.networkInterfaces();
-    for (const name in nifs) {
-        /** --- 当前网卡 --- */
-        const card = nifs[name];
-        if (!card) {
+    let i = 0;
+    while (i < 5) {
+        const nifs = os.networkInterfaces();
+        if (!Object.keys(nifs).length) {
+            ++i;
+            await lCore.sleep(50);
             continue;
         }
-        if (card[0].internal) {
-            continue;
+        for (const name in nifs) {
+            /** --- 当前网卡 --- */
+            const card = nifs[name];
+            if (!card) {
+                continue;
+            }
+            if (card[0].internal) {
+                continue;
+            }
+            const item = {
+                'name': name,
+                'mac': '',
+                'iPv4': '',
+                'iPv6': ''
+            };
+            for (const it of card) {
+                if (!item.mac) {
+                    item.mac = it.mac;
+                }
+                if (it.family === 'IPv4') {
+                    item.iPv4 = it.address;
+                }
+                else {
+                    item.iPv6 = it.address;
+                }
+            }
+            result.push(item);
         }
-        const item = {
-            'name': name,
-            'mac': '',
-            'iPv4': '',
-            'iPv6': ''
-        };
-        for (const it of card) {
-            if (!item.mac) {
-                item.mac = it.mac;
-            }
-            if (it.family === 'IPv4') {
-                item.iPv4 = it.address;
-            }
-            else {
-                item.iPv6 = it.address;
-            }
-        }
-        result.push(item);
+        break;
     }
     return result;
 }
 
+/**
+ * --- 扫描发生关联的局域网 IP ---
+ */
 export async function scan(): Promise<Array<{
     'mac': string;
     'iPv4': string;
