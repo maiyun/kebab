c00825b0a89d5e6c66b5c12c52d0852b0cafd992
han <zohegs@gmail.com>
2025-09-30 14:13:37 +0800

    3.1.13

--- sys/child.ts
+++ sys/child.ts
@@ -613,7 +613,7 @@ function getVhostByHostname(hostname: string): kebab.IVhost | null {
             }
             else if (domain.includes('*')) {
                 // --- 通配符匹配 ---
-                domain = domain.replace(/\*/, '.+?').replace(/\./, '\\.');
+                domain = domain.replace(/\./g, '\\.').replace(/\*/, '.+?');
                 if (new RegExp(`^${domain}$`).test(hostname)) {
                     vSub = vhost;
                 }
